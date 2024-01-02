2e861b4d2acc348eeaca855f7a2a408e4d13c89f
kkasans <150230645+kkasans@users.noreply.github.com>
2024-01-02 11:00:38 +0900

    Update live.js

--- js/live.js
+++ js/live.js
@@ -1,17 +1,21 @@
 // toggle
-function openNav(){
+function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 // close
-function closeNav(){
+function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 // popup
 $(function(){
     $(".btn").on("click",function(){
-        $("#popup").fadeIn();
+        $("#popup, .bg").fadeIn();
     });
     $(".close").on("click",function(){
-        $("#popup").fadeOut();
+        $("#popup, .bg").fadeOut();
+    });
+    // 배경 클릭시 팝업과 배경 사라짐
+    $("#popup, .bg").on("click",function(){
+        $("#popup, .bg").fadeOut(); 
     });
 });
